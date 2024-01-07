4bdf34a277151a6b16958d867f32b42f6388d1a3
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-07 14:00:16 +0200

    Progress Lesson

--- code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
+++ code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
@@ -1,33 +1,29 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import { CORE_CONCEPTS } from "./data"
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <section id="core-concepts">
+                <h2>Core Concepts</h2>
+                <ul>
+                    <CoreConcept {...CORE_CONCEPTS[0]} />
+                    <CoreConcept {...CORE_CONCEPTS[1]} />
+                    <CoreConcept {...CORE_CONCEPTS[2]} />
+                    <CoreConcept
+                        title={CORE_CONCEPTS[3].title}
+                        description={CORE_CONCEPTS[3].description}
+                        img={CORE_CONCEPTS[3].image}
+                    />
+                </ul>
+            </section>
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ code/03 React Essentials/Self Progress/src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+// export default function TabButton({ children }) {
+//     return <li><button>{children}</button></li>
+// };
+
+export default function TabButton({ label }) {
+    return <li><button>{label}</button></li>
+};
