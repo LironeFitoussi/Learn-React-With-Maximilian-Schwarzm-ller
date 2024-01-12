a1bee88e0f66b782ef52f0d6b0b40ff37b40a878
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-12 15:10:59 +0200

    630

--- Section 29/starting-project/src/App.jsx
+++ Section 29/starting-project/src/App.jsx
@@ -2,7 +2,25 @@ import Post from "./components/Post/Post.jsx";
 
 function App() {
   return (
-    <Post />
+    <main>
+      <Post
+        author='Lirone'
+        body='react is awesome'
+      />
+      <Post
+        author='dfgsdg'
+        body='dsfgsdfgsdrgservbdf'
+      />
+      <Post
+        author='MAmy'
+        body='אני רוצה מכונית'
+      />
+      <Post
+        author='Ron'
+        body='I lovve Crypto'
+      />
+      <Post />
+    </main>
   );
 }
 
