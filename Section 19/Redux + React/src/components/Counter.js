12036ee6c281b9424675e745f0f1075e58b48184
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-05-14 23:31:03 +0300

    297

--- Section 19/Redux + React/src/components/Counter.js
+++ Section 19/Redux + React/src/components/Counter.js
@@ -10,6 +10,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "INCREMENT" });
@@ -19,14 +20,21 @@ const Counter = () => {
     dispatch({ type: "DECREMENT" });
   };
 
-  const toggleCounterHandler = () => {};
+  const increaseHandler = () => {
+    dispatch({ type: "INCREASE", amount: 10 });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      <div className={classes.value}>{show ? counter : "---"}</div>
       <div>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>Increment by 10</button>
         <button onClick={incrementHandler}>Increment</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- Section 19/Redux + React/src/store/index.js
+++ Section 19/Redux + React/src/store/index.js
@@ -1,11 +1,21 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (
+  state = {
+    counter: 0,
+    showCounter: true,
+  },
+  action
+) => {
   switch (action.type) {
     case "INCREMENT":
       return { ...state, counter: state.counter + 1 };
     case "DECREMENT":
       return { ...state, counter: state.counter - 1 };
+    case "INCREASE":
+      return { ...state, counter: state.counter + action.amount };
+    case "TOGGLE":
+      return { ...state, showCounter: !state.showCounter };
     default:
       return state;
   }
