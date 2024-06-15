5f84fb456f2d52fc54560bd44b6d757905421141
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-06-16 00:21:23 +0300

    feat: Add error handling and loading state to AvailablePlaces component
    The code changes in this commit add error handling and a loading state to the AvailablePlaces component. This allows for better user experience by displaying an error message when there is an issue fetching the available places and showing a loading message while the data is being fetched.

--- Section 15/src/components/AvailablePlaces.jsx
+++ Section 15/src/components/AvailablePlaces.jsx
@@ -1,24 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchAvailablePlaces() {
-      const res = await fetch('http://localhost:3000/places');
-      const data = await res.json();
-      setAvailablePlaces(data.places);
+      try {
+        const res = await fetch('http://localhost:3000/placessss');
+        const data = await res.json();
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch available places');
+        }
+
+        setAvailablePlaces(data.places);
+      } catch (error) {
+        console.error('Error fetching available places:', error);
+        setError(error);
+      }
+
+      setIsLoading(false);
     }
 
     fetchAvailablePlaces();
   }, []);
 
+
+  if (error) {
+    return (
+      <Error title="An error occurred!" message={error.message || 'Could not fetch places, please try again later.'}/>
+    );
+  }
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
-      fallbackText="No places available."
+      isLoading={isLoading}
+      loadingText="Loading available places..."
+      fallbackText='No places available.'
       onSelectPlace={onSelectPlace}
     />
   );
