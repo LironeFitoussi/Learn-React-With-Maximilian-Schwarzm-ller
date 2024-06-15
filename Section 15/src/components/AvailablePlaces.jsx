b3bcc8c8a500b070b74ffa95873d538ad46da414
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-06-15 23:57:57 +0300

    fixed space

--- Section 15/src/components/AvailablePlaces.jsx
+++ Section 15/src/components/AvailablePlaces.jsx
@@ -7,7 +7,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost: 3000/places')
+    fetch('http://localhost:3000/places')
       .then((response) => response.json())
       .then(({places}) => {
         setAvailablePlaces(places);
