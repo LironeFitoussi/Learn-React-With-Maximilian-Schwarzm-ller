16db205015849edf6cc16acf0697c5b3ad19931b
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-06-15 23:57:28 +0300

    fetching data set

--- Section 15/src/components/AvailablePlaces.jsx
+++ Section 15/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
+import React, { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
+const places = localStorage.getItem('places') || [];
+
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost: 3000/places')
+      .then((response) => response.json())
+      .then(({places}) => {
+        setAvailablePlaces(places);
+        localStorage.setItem('places', JSON.stringify(places));
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
