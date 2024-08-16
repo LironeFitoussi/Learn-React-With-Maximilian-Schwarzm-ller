1397a9ad20c6e5fd550b7b61ac7a3e6e78f7e26a
LironeFitoussi <lironefit@gmail.com>
2024-08-16 15:03:27 +0300

    Deleting St

--- Section 24/src/components/Events/EditEvent.jsx
+++ Section 24/src/components/Events/EditEvent.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@tanstack/react-query';
 
 // Import the fetchEvent function
-import { fetchEvent } from '../../util/http.js';
+import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
@@ -18,7 +18,38 @@ export default function EditEvent() {
     queryFn: ({signal}) => fetchEvent({signal, id}),
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      // 1. get the current event data from the cache
+      const newEvent = data.event;
+
+      // 2. cancel any outgoing queries with the same key
+      await queryClient.cancelQueries({ queryKey: ['events', { id }] });
+
+      // 3. save the previous event data in a snapshot
+      const previousEvent = queryClient.getQueryData(['events', { id }]);
+
+      // 4. set the new event data in the cache
+      queryClient.setQueryData(['events', { id }], newEvent);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      // 5. rollback to the previous event data
+      queryClient.setQueryData(['events', { id }], context.previousEvent);
+    },
+
+    // 6. once the mutation is done, invalidate the event query
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['events', { id }] });
+    }
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id, event: formData });
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
