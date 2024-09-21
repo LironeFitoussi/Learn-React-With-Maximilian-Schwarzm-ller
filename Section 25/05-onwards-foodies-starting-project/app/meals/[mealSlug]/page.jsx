78a0d96ddc9bc762c81a8cd65e6a491e8e6ff1da
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-09-21 12:52:09 -0700

    Refactor getMeals function to use async/await and reduce network request time

--- Section 25/05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.jsx
+++ Section 25/05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import classes from './page.module.css';
 
-const MealPage = ({ params: { mealSlug }}) => {
+// DB Functions
+import { getMeal } from '@/lib/meals';
+
+// Next.js Components
+import Image from 'next/image';
+
+// Next.js Functions
+import { notFound } from 'next/navigation';
+const MealPage = async ({ params: { mealSlug }}) => {
+    const meal = await getMeal(mealSlug);
+
+    if (!meal) {
+        return notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br>');
+
     return (
-        <div>
-            <h1>Meal: {mealSlug}</h1>
-            <p>Details about the meal will go here.</p>
-        </div>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} fill/>
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a herf={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+            </main>
+        </>
     );
 };
 
