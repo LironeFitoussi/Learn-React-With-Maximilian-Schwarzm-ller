697f1e6065be225c7702af30abf18375920dc52d
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-01 12:20:15 +0300

    500

--- Section 26/starting-project/src/components/Modal.jsx
+++ Section 26/starting-project/src/components/Modal.jsx
@@ -2,21 +2,22 @@ import { createPortal } from 'react-dom';
 import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
-  const hiddenAnimationState = { opacity: 0, y: 50 };
+  // const hiddenAnimationState = { opacity: 0, y: 50 };
 
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
         variants={{
-          hidden: hiddenAnimationState,
+          hidden: { opacity: 0, y: 30 },
           visible: { opacity: 1, y: 0 },
         }}
         initial="hidden"
         animate="visible"
         exit="hidden"
         open 
-        className="modal">
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
       </motion.dialog>
