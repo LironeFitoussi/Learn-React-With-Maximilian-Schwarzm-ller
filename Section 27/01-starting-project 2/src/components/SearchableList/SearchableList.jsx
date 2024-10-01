0e55f271711d1c6437773509e1e974e37a552994
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-01 16:55:27 +0300

    520 - Section 27 Done

--- Section 27/01-starting-project 2/src/components/SearchableList/SearchableList.jsx
+++ Section 27/01-starting-project 2/src/components/SearchableList/SearchableList.jsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function SearchableList({ items, itemKeyFn, children }) {
+    const lastChange = useRef(null)
     const [searchTerm, setSearchTerm] = useState('');
 
     // console.log(items);
-    
-    function handleSearch(event) {
-        setSearchTerm(event.target.value);
-    }
-
     const searchResults = items.filter((item) => 
         JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
     );
+    
+    // Debounce the search
+    function handleSearch(event) {
+        if (lastChange.current) {
+            clearTimeout(lastChange.current);
+        }
+
+        lastChange.current = setTimeout(() => {
+            lastChange.current = null;
+            setSearchTerm(event.target.value);
+        }, 500);
+    }
 
     return (
         <div className="searchable-list">
