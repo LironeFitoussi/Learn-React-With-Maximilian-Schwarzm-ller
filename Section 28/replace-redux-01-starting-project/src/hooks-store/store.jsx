5325466285d01e5154dc125b7375b276865ab4e7
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-01 22:45:34 +0300

    527

---
+++ Section 28/replace-redux-01-starting-project/src/hooks-store/store.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = () => {
+    const setState = useState(globalState)[1];
+    
+    useEffect(() => {
+        listeners.push(setState);
+    
+        return () => {
+            listeners = listeners.filter(li => li !== setState);
+        };
+    }, [setState]);
+    
+    // return [globalState, dispatch];
+};
