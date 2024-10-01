39afcef6098311d7ead66c870b895a88fcedebea
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-01 16:24:48 +0300

    515

--- Section 27/01-starting-project 2/src/App.jsx
+++ Section 27/01-starting-project 2/src/App.jsx
@@ -6,17 +6,17 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title id="experience" className="accordion-item-title">We deliver results</Accordion.Title>
-            <Accordion.Content id="experience" className="accordion-item-content">
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">We deliver results</Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>Our team has a proven track record of delivering results. We&apos;ve helped hundreds of clients achieve their goals and grow their businesses.</p>
               </article>
             </Accordion.Content> 
           </Accordion.Item>
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title id="something" className="accordion-item-title">We&apos;re experts in our field</Accordion.Title>
-            <Accordion.Content id="something" className="accordion-item-content">
+          <Accordion.Item id="something" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">We&apos;re experts in our field</Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>Our team is made up of experts in their respective fields. We have years of experience and a wealth of knowledge that we bring to every project.</p>
               </article>
--- Section 27/01-starting-project 2/src/components/Accordion/AccordionContent.jsx
+++ Section 27/01-starting-project 2/src/components/Accordion/AccordionContent.jsx
@@ -1,6 +1,7 @@
 import { useAccordionContext } from "./Accordion"
-
-export default function AccordionContent({ id, children, className }) {
+import { useAccordionItemContext } from "./AccordionItem";
+export default function AccordionContent({ children, className }) {
+    const id = useAccordionItemContext();
     const { openItemId } = useAccordionContext();
     const isOpen = openItemId === id;
     return (
