c9ed5932cc02e2b0f91e209d70ed36c9163a2825
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-02 11:59:09 +0300

    561

---
+++ Section 30/react-ts/src/components/Todos.tsx
@@ -0,0 +1,11 @@
+function Todos() {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Learn TypeScript</li>
+            <li>Build a project</li>
+        </ul>
+    );
+}
+
+export default Todos;
