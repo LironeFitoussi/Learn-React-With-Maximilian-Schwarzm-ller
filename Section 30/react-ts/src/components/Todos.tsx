a4cd88fe88fd8cd8b83807a68dd424ff1f5a1785
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-02 12:19:18 +0300

    564

--- Section 30/react-ts/src/components/Todos.tsx
+++ Section 30/react-ts/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
 
 const Todos: React.FC<{ items: Todo[]}> = (props) => {
     return (
         <ul>
             {props.items.map((todo) => (
-                <li key={todo.id}>{todo.text}</li>
+                <TodoItem key={todo.id} text={todo.text} />
             ))}
         </ul>
     );
