88e05976774ce665b3ee0aad3401b04035b83572
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-02 10:40:52 +0300

    552

---
+++ Section 30/01-starting-project 2/basics.ts
@@ -0,0 +1,22 @@
+// Primitives: number, string, boolean
+
+// More Complex Types: Arrays, Objects
+
+// Function Types, parameters
+
+// Primitives
+let age: number;    // number
+// age = 12;   // correct
+// age = '12'; // wrong
+
+let userName: string;   // string
+// userName = 'Max'; // correct
+// userName = 12; // wrong
+
+let isInstructor: boolean;  // boolean
+// isInstructor = true; // correct
+// isInstructor = 'true'; // wrong
+
+let hobbies: null;  // null
+// hobbies = null; // correct
+// hobbies = 'null'; // wrong
