9ae0867d68a949dd95f8547fbe0b4cd690541e72
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-02 12:27:58 +0300

    566

--- Section 30/react-ts/src/components/NewTodo.tsx
+++ Section 30/react-ts/src/components/NewTodo.tsx
@@ -1,11 +1,24 @@
+import { useRef } from "react";
+
 const NewTodo: React.FC = () => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null)
+
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
-    }
+        const enteredText = todoTextInputRef.current!.value;
+        
+        if (enteredText.trim().length === 0) {
+            // throw an error
+            return;
+        }
+
+        
+    };
+
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor="text"></label>
-            <input type="text" id="text" />
+            <input type="text" id="text" ref={todoTextInputRef} />
             <button type="submit">Add Todo</button>
         </form>
     );
